b428e592d601074ac8f58d855d01bf15e6a57769
Mohammad Dahamshi <mohammad.dahamshi@gmail.com>
2025-07-30 15:37:12 +0300

    fixes a bug

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,4 +1,4 @@
-import { getAllUsers } from '../storage/index.js';
+import { getAllUsers } from '../storages/index.js';
 
 export function getHome(req, res) {
   res.render('pages/home', { title: 'Home' });
